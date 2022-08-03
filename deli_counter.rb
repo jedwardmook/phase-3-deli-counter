e5b69138afdbdaa343bd9c00efb52373b721a9b4
JedwardMook <jedwardmook@yahoo.com>
2022-08-03 14:40:55 -0500

    commit tree 99c1a2a05717837e4c49c81c8da469bbfae77d17

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def line(array)
+    if array.length == 0
+        puts "The line is currently empty."
+    else
+        newArray = array.each_with_index.map do |person, index|
+            "#{index + 1}. #{person}"
+        end
+        newNewArray = newArray.join(" ")
+        puts "The line is currently: #{newNewArray}"
+    end
+end
+
+def take_a_number(array, string)
+   puts "Welcome, #{string}. You are number #{array.length + 1} in line."
+   array << string 
+end
+
+def now_serving(array)
+    if array.length > 0
+            puts "Currently serving #{array[0]}."
+            array.shift    
+    else
+        puts "There is nobody waiting to be served!"
+    end
+end
+
+
